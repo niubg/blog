89602e9827c36873881b3b77d2cef5e36e4d5e84
binggang.niu <binggang.niu@tadu.com>
2021-03-09 20:38:42 +0800

    fix：配置调整

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,22 +4,48 @@ const sideBarConfig = require("./sideBar");
 
 module.exports = config({
   title: "niubg",
-  description: "A vuepress theme with tons of features✨",
+  // description: "A vuepress theme with tons of features✨",
   dest: "./dist",
 
+  head: [
+    [
+      "meta",
+      {
+        name: "google-site-verification",
+        content: "qG3soux9jAKB4Q_DYf7yj1p5cEIuib6yG4zDhpmv2_E",
+      },
+    ],
+    [
+      "script",
+      { src: "https://cdn.jsdelivr.net/npm/react/umd/react.production.min.js" },
+    ],
+    [
+      "script",
+      {
+        src:
+          "https://cdn.jsdelivr.net/npm/react-dom/umd/react-dom.production.min.js",
+      },
+    ],
+    ["script", { src: "https://cdn.jsdelivr.net/npm/vue/dist/vue.min.js" }],
+    [
+      "script",
+      { src: "https://cdn.jsdelivr.net/npm/@babel/standalone/babel.min.js" },
+    ],
+  ],
 
   locales: {
-    "/": {
-      title: "niubg",
-      lang: 'zh-CN',
-      description: "一个具有强大功能的 vuepress 主题✨",
-    },
+    // "/": {
+    //   title: "niubg",
+    //   lang: 'zh-CN',
+    //   description: "一个具有强大功能的 vuepress 主题✨",
+    // },
   },
 
   themeConfig: {
-    logo: "/logo.svg",
+    // logo: "/logo.svg",
     // hostname: "https://vuepress-theme-hope.github.io",
     author: "niubg",
+    baseLang: 'zh-CN',
     iconPrefix: "vuepress-",  
     repoDisplay: false,
 
@@ -27,29 +53,29 @@ module.exports = config({
     nav: navBarConfig.zh,
     sidebar: sideBarConfig.zh,
 
-    algolia: {
-      apiKey: "4deb442097fb6a05638adf10ef86e222",
-      indexName: "mrhope_vuepress-theme",
-    },
+    // algolia: {
+    //   apiKey: "4deb442097fb6a05638adf10ef86e222",
+    //   indexName: "mrhope_vuepress-theme",
+    // },
 
     footer: {
       display: true,
       copyright: "MIT Licensed | Copyright © 2021-present niubg",
     },
 
-    comment: {
-      type: "valine",
-      appId: "2vSLKb0SqFKKWEgrOPGy3sp1-gzGzoHsz",
-      appKey: "vma8Ewk61WeNkI81O3CGpT2i",
-    },
+    // comment: {
+    //   type: "valine",
+    //   appId: "2vSLKb0SqFKKWEgrOPGy3sp1-gzGzoHsz",
+    //   appKey: "vma8Ewk61WeNkI81O3CGpT2i",
+    // },
 
     copyright: {
       status: "local",
     },
 
-    lastUpdate: {
-      timezone: "Asia/Shanghai",
-    },
+    // lastUpdate: {
+    //   timezone: "Asia/Shanghai",
+    // },
 
     mdEnhance: {
       enableAll: true,
@@ -58,103 +84,103 @@ module.exports = config({
       },
     },
 
-    pwa: {
-      favicon: "/favicon.ico",
-      themeColor: "#46bd87",
-      cachePic: true,
-      apple: {
-        icon: "/assets/icon/apple-icon-152.png",
-        statusBarColor: "black",
-      },
-      msTile: {
-        image: "/assets/icon/ms-icon-144.png",
-        color: "#ffffff",
-      },
-      manifest: {
-        icons: [
-          {
-            src: "/assets/icon/chrome-mask-512.png",
-            sizes: "512x512",
-            purpose: "maskable",
-            type: "image/png",
-          },
-          {
-            src: "/assets/icon/chrome-mask-192.png",
-            sizes: "192x192",
-            purpose: "maskable",
-            type: "image/png",
-          },
-          {
-            src: "/assets/icon/chrome-512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-          {
-            src: "/assets/icon/chrome-192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-        ],
-        shortcuts: [
-          {
-            name: "Guide",
-            short_name: "Guide",
-            url: "/guide/",
-            icons: [
-              {
-                src: "/assets/icon/guide-maskable.png",
-                sizes: "192x192",
-                purpose: "maskable",
-                type: "image/png",
-              },
-              {
-                src: "/assets/icon/guide-monochrome.png",
-                sizes: "192x192",
-                purpose: "monochrome",
-                type: "image/png",
-              },
-            ],
-          },
-          {
-            name: "Config",
-            short_name: "Config",
-            url: "/config/",
-            icons: [
-              {
-                src: "/assets/icon/config-maskable.png",
-                sizes: "192x192",
-                purpose: "maskable",
-                type: "image/png",
-              },
-              {
-                src: "/assets/icon/config-monochrome.png",
-                sizes: "192x192",
-                purpose: "monochrome",
-                type: "image/png",
-              },
-            ],
-          },
-          {
-            name: "Basic",
-            short_name: "Basic",
-            url: "/basic/",
-            icons: [
-              {
-                src: "/assets/icon/basic-maskable.png",
-                sizes: "192x192",
-                purpose: "maskable",
-                type: "image/png",
-              },
-              {
-                src: "/assets/icon/basic-monochrome.png",
-                sizes: "192x192",
-                purpose: "monochrome",
-                type: "image/png",
-              },
-            ],
-          },
-        ],
-      },
-    },
+    // pwa: {
+    //   favicon: "/favicon.ico",
+    //   themeColor: "#46bd87",
+    //   cachePic: true,
+    //   apple: {
+    //     icon: "/assets/icon/apple-icon-152.png",
+    //     statusBarColor: "black",
+    //   },
+    //   msTile: {
+    //     image: "/assets/icon/ms-icon-144.png",
+    //     color: "#ffffff",
+    //   },
+    //   manifest: {
+    //     icons: [
+    //       {
+    //         src: "/assets/icon/chrome-mask-512.png",
+    //         sizes: "512x512",
+    //         purpose: "maskable",
+    //         type: "image/png",
+    //       },
+    //       {
+    //         src: "/assets/icon/chrome-mask-192.png",
+    //         sizes: "192x192",
+    //         purpose: "maskable",
+    //         type: "image/png",
+    //       },
+    //       {
+    //         src: "/assets/icon/chrome-512.png",
+    //         sizes: "512x512",
+    //         type: "image/png",
+    //       },
+    //       {
+    //         src: "/assets/icon/chrome-192.png",
+    //         sizes: "192x192",
+    //         type: "image/png",
+    //       },
+    //     ],
+    //     shortcuts: [
+    //       {
+    //         name: "Guide",
+    //         short_name: "Guide",
+    //         url: "/guide/",
+    //         icons: [
+    //           {
+    //             src: "/assets/icon/guide-maskable.png",
+    //             sizes: "192x192",
+    //             purpose: "maskable",
+    //             type: "image/png",
+    //           },
+    //           {
+    //             src: "/assets/icon/guide-monochrome.png",
+    //             sizes: "192x192",
+    //             purpose: "monochrome",
+    //             type: "image/png",
+    //           },
+    //         ],
+    //       },
+    //       {
+    //         name: "Config",
+    //         short_name: "Config",
+    //         url: "/config/",
+    //         icons: [
+    //           {
+    //             src: "/assets/icon/config-maskable.png",
+    //             sizes: "192x192",
+    //             purpose: "maskable",
+    //             type: "image/png",
+    //           },
+    //           {
+    //             src: "/assets/icon/config-monochrome.png",
+    //             sizes: "192x192",
+    //             purpose: "monochrome",
+    //             type: "image/png",
+    //           },
+    //         ],
+    //       },
+    //       {
+    //         name: "Basic",
+    //         short_name: "Basic",
+    //         url: "/basic/",
+    //         icons: [
+    //           {
+    //             src: "/assets/icon/basic-maskable.png",
+    //             sizes: "192x192",
+    //             purpose: "maskable",
+    //             type: "image/png",
+    //           },
+    //           {
+    //             src: "/assets/icon/basic-monochrome.png",
+    //             sizes: "192x192",
+    //             purpose: "monochrome",
+    //             type: "image/png",
+    //           },
+    //         ],
+    //       },
+    //     ],
+    //   },
+    // },
   },
 });
