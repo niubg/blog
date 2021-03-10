a423beb52fcd9408466fe58b82355187583f5f72
binggang.niu <binggang.niu@tadu.com>
2021-03-10 18:01:20 +0800

    fix

--- server/blog-start.sh
+++ server/blog-start.sh
@@ -6,10 +6,12 @@ cd /home/blog/view
 ## 拉取最新代码
 echo '开始拉去最新代码'
 git pull
-echo '进入项目目录'
-cd blog
+
+## 因为服务器内存低，无法使用服务器编译，故取消编译命令
+#echo '进入项目目录'
+#cd blog
 ## 打包静态文件
-echo '开始构建静态文件'
-yarn install
-yarn run build
-echo '最新文章处理完成'
+#echo '开始构建静态文件'
+#yarn install
+#yarn run build
+#echo '最新文章处理完成'
